d63552514dd624b63594b144a81c1d6af46a6608
Daniel <ddblyons@gmail.com>
2018-12-01 13:44:12 -0600

    trying to get the main file to freaking upload to the git tree

--- espressgo-app/lib/main.dart
+++ espressgo-app/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text('Please work.'),
         ),
         body: Center(
           child: Text('Hello World'),
